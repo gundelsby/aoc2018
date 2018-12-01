281f479924ce165f122c95c1cca313d750f4f32b
Ola Christian Gundelsby <lug00ber@gmail.com>
2018-12-01 11:38:39 +0100

    Day 1 done and dusted

--- solutions/01.js
+++ solutions/01.js
@@ -1,9 +1,25 @@
 const fs = require("fs");
 const input = fs.readFileSync("data/01-input-1.txt", "utf-8").split("\n");
-let firstRepeated;
+const freqHistory = new Set();
+let foundDuplicate = false;
+let frequency = 0;
 
-const frequency = input.reduce((freq, current) => {
-  return freq + Number(current);
-}, 0);
+// Part 1
+// const frequency = input.reduce((freq, current) => {
+//   return freq + Number(current);
+// }, 0);
+
+while (!foundDuplicate) {
+  for (let i = 0; i < input.length; i++) {
+    frequency += Number(input[i]);
+
+    if (freqHistory.has(frequency)) {
+      foundDuplicate = true;
+      break;
+    } else {
+      freqHistory.add(frequency);
+    }
+  }
+}
 
 console.log(`frequency: ${frequency}`);
