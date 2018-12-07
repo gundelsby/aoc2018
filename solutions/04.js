5661d8f6c4d6cab9a88618dcf0d21e05da175608
Ola Christian Gundelsby <lug00ber@gmail.com>
2018-12-07 01:49:42 +0100

    More work on day 4

--- solutions/04.js
+++ solutions/04.js
@@ -1,59 +1,62 @@
-const fs = require("fs");
-const input = fs.readFileSync("data/04-input.txt", "utf-8").split("\n");
+const fs = require('fs');
+const input = fs.readFileSync('data/04-input.txt', 'utf-8').split('\n');
 
 const actionMap = {
   BEGINS_SHIFT: 'begins shift',
   FALLS_ASLEEP: 'falls asleep',
   WAKES_UP: 'wakes up'
-}
-const guards = {};
+};
 
 const log = parseInput();
 log.sort((a, b) => a.timestamp - b.timestamp);
 
-let guard = null;
-for (let i = 0; i < log.length; i++) {
-  if (log[i].guard) {
-    guard = log[i].guard;
-    guards[log[i].guard] = {}
-  } else {
-    log[guard] = guard;
+const guards = {};
+
+let guardOnDuty;
+let lastAction;
+let delta;
+
+log.forEach((logItem) => {
+  if (logItem.action === actionMap.BEGINS_SHIFT) {
+    guardOnDuty = logItem.guard;
   }
-}
-
-
+});
 
 function parseInput() {
   const reLogLine = /^\[(\d+-\d+-\d+ \d+:\d+)\]\s(.*)$/;
   const reGuardId = /#(\d+)/;
-  // [1518-07-12 00:49] falls asleep
-  // [1518-10-27 00:45] falls asleep
-  // [1518-07-04 23:59] Guard #1171 begins shift
-  // [1518-09-21 00:56] wakes up
-  // [1518-08-16 00:41] falls asleep
-  // Timestamps are written using year-month-day hour:minute format.
-  // The guard falling asleep or waking up is always the one whose shift most recently started.
-  // Because all asleep/awake times are during the midnight hour (00:00 - 00:59),
-  // only the minute portion (00 - 59) is relevant for those events.
+
   return input
     .map((logLine) => {
       const parts = reLogLine.exec(logLine);
+
       return {
         timestamp: new Date(parts[1]),
         entry: parts[2]
-      }
+      };
     })
     .map((logItem) => {
-      const parts = reGuardId.exec(logItem.entry)
+      const parts = reGuardId.exec(logItem.entry);
       if (parts) {
-        logItem.guard = parts[1]
+        logItem.guard = parts[1];
       }
       Object.keys(actionMap).forEach((action) => {
         if (logItem.entry.indexOf(actionMap[action]) > -1) {
-          logItem.action = action;
+          logItem.action = actionMap[action];
         }
-      })
+      });
 
-      return logItem
+      return logItem;
+    })
+    .map((logItem) => {
+      if (logItem.action === actionMap.BEGINS_SHIFT) {
+        logItem.timestamp.setUTCMinutes(0);
+
+        if (logItem.timestamp.getUTCHours() === 23) {
+          logItem.timestamp.setUTCHours(24);
+        }
+      }
+
+      return logItem;
     });
 }
