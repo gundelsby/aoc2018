39e7e2c2bc8238ac4dbff66b4dec88c6872853da
Ola Christian Gundelsby <lug00ber@gmail.com>
2018-12-11 00:51:49 +0100

    Day 4, done and dusted

--- solutions/04.js
+++ solutions/04.js
@@ -8,25 +8,92 @@ const actionMap = {
 };
 
 const log = parseInput();
-log.sort((a, b) => a.timestamp - b.timestamp);
 
-const guards = {};
+const guards = analyzeLog(log);
 
-let guardOnDuty;
-let lastAction;
-let delta;
+const lazy = findLazy(guards);
+const laziestMinute = guards[lazy].indexOf(Math.max(...guards[lazy]));
 
-log.forEach((logItem) => {
-  if (logItem.action === actionMap.BEGINS_SHIFT) {
-    guardOnDuty = logItem.guard;
+console.log(
+  `Biggest sleeper is ${lazy}, most frequently asleep at 00:${laziestMinute}. Result: ${lazy *
+    laziestMinute}`
+);
+
+const predictable = findPredictable(guards);
+const mostFrequentMinute = guards[predictable].indexOf(Math.max(...guards[predictable]));
+
+console.log(
+  `Most predictable sleeper is ${predictable}, most frequently asleep at 00:${mostFrequentMinute}. Result: ${predictable *
+    mostFrequentMinute}`
+);
+
+function findPredictable(guards) {
+  return Object.keys(guards).reduce((predictable, current) => {
+    return Math.max(...guards[current]) > Math.max(...guards[predictable]) ? current : predictable;
+  });
+}
+
+function findLazy(guards) {
+  return Object.keys(guards).reduce((laziest, current) => {
+    return guards[current].reduce((sum, current) => sum + current) >
+      guards[laziest].reduce((sum, current) => sum + current)
+      ? current
+      : laziest;
+  });
+}
+
+function analyzeLog(log) {
+  const guards = {};
+
+  let guardOnDuty;
+  let lastAction;
+  let delta;
+
+  log.forEach((logItem, idx) => {
+    switch (logItem.action) {
+      case actionMap.BEGINS_SHIFT:
+        if (lastAction === actionMap.FALLS_ASLEEP) {
+          guards[guardOnDuty] = recordSleep(
+            guards[guardOnDuty],
+            lastAction.timestamp.getMinutes(),
+            60
+          );
+        }
+        guardOnDuty = logItem.guard;
+        break;
+      case actionMap.WAKES_UP:
+        if (!guardOnDuty) {
+          console.log(`[${idx}] Wake up with no guard on duty:`, logItem);
+          break;
+        }
+        guards[guardOnDuty] = recordSleep(
+          guards[guardOnDuty],
+          lastAction.timestamp.getMinutes(),
+          logItem.timestamp.getMinutes()
+        );
+        break;
+    }
+    lastAction = logItem;
+  });
+
+  return guards;
+}
+
+function recordSleep(record, start, end) {
+  record = record || new Array(60).fill(0);
+  for (let i = start; i < end; i++) {
+    record[i] += 1;
   }
-});
+
+  return record;
+}
 
 function parseInput() {
   const reLogLine = /^\[(\d+-\d+-\d+ \d+:\d+)\]\s(.*)$/;
   const reGuardId = /#(\d+)/;
 
   return input
+    .sort()
     .map((logLine) => {
       const parts = reLogLine.exec(logLine);
 
@@ -49,12 +116,9 @@ function parseInput() {
       return logItem;
     })
     .map((logItem) => {
-      if (logItem.action === actionMap.BEGINS_SHIFT) {
-        logItem.timestamp.setUTCMinutes(0);
-
-        if (logItem.timestamp.getUTCHours() === 23) {
-          logItem.timestamp.setUTCHours(24);
-        }
+      if (logItem.timestamp.getHours() === 23) {
+        logItem.timestamp.setMinutes(0);
+        logItem.timestamp.setHours(24);
       }
 
       return logItem;
