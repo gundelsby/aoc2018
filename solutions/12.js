d038941af989d67b04b18cbd8953596802461f3d
Ola Christian Gundelsby <lug00ber@gmail.com>
2018-12-13 01:30:15 +0100

    Day 12, done and dusted

--- solutions/12.js
+++ solutions/12.js
@@ -8,38 +8,70 @@ const input = fs
 const initialState = parseInitialState(input[0]);
 const ruleSet = createRuleSet(input.slice(2));
 
-console.log(`[1]:`, calcPotSum(runSimulation(initialState, ruleSet, 20)));
-// console.log(`[2]:`, calcPotSum(runSimulation(initialState, ruleSet, 50000000000)));
+console.log(`[1]:`, calcPotSum(runSimulation(initialState, ruleSet, 20)[20]));
 
-function calcPotSum ({state, addedLeft}) {
+const diffs = runSimulation(initialState, ruleSet, 170)
+  .map(calcPotSum)
+  .map((score, index, arr) => {
+    if (!arr[index - 1]) {
+      return score;
+    }
+
+    return Math.abs(score - arr[index - 1]);
+  });
+
+console.log(diffs.map((diff, index) => `${index}: ${diff}`).slice(-5));
+
+// all generations after 168 increases with 75 plants;
+
+const calcTarget = 50000000000; // number of generations to calculate for
+const lastUnique = 168;
+
+const score =
+  calcPotSum(runSimulation(initialState, ruleSet, lastUnique)[lastUnique]) +
+  75 * (calcTarget - lastUnique);
+
+console.log('[2]:', score);
+
+function calcPotSum({ state, addedLeft }) {
   return state.split('').reduce((sum, pot, index) => {
     const value = pot === '#' ? index - addedLeft : 0;
     return sum + value;
-  }, 0)
+  }, 0);
 }
 
-function runSimulation (state, ruleSet, generationCount) {
+function runSimulation(state, ruleSet, generationCount) {
+  const states = [{ state, addedLeft: 0 }];
   const padPots = '....';
-  
+
   for (let i = 0; i < generationCount; i++) {
-    state = padPots + state + padPots; // add empty pots to the sides for proper rule eval
+    let addedLeft = states[i].addedLeft;
+
+    const padSize = 4 - state.indexOf('#');
+    if (padSize > 0) {
+      state = padPots.slice(-padSize) + state;
+      addedLeft += padSize;
+    }
+    state = state.padEnd(state.lastIndexOf('#') + 4, '.');
+
     state = applyRules(state, ruleSet);
+    states.push({
+      state,
+      addedLeft
+    });
   }
 
-  return {
-    state,
-    addedLeft: padPots.length * generationCount
-  };
+  return states;
 }
 
-function applyRules (state, ruleSet) {
-  const newState = ['.','.']; // just to not fuck up the padding calc
+function applyRules(state, ruleSet) {
+  const newState = ['.', '.'];
 
   for (let i = 2; i < state.length - 2; i++) {
-    const sequence = state.substring(i-2, i+3);
+    const sequence = state.substring(i - 2, i + 3);
     const newValue = ruleSet.reduce((value, current) => {
-      return current.pattern === sequence ? current.value : value
-    }, '.')
+      return current.pattern === sequence ? current.value : value;
+    }, '.');
     newState.push(newValue);
   }
 
