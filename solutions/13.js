fa0269d170deba16e431a76bd0384451675ff078
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2018-12-13 18:30:59 +0100

    Day 13, carts driving off the grid :S

--- solutions/13.js
+++ solutions/13.js
@@ -8,7 +8,37 @@ const map = input
   });
 
 const carts = createCarts(map);
-console.log(carts);
+let collisionPosition;
+let moves = 0;
+
+while (!collisionPosition) {
+  console.log(`Move #${moves++}`);
+  collisionPosition = checkCollisions(carts);
+  carts.sort(cmpCartPositions);
+  carts.forEach((cart) => cart.move());
+}
+
+console.log(collisionPosition);
+
+function cmpCartPositions(a, b) {
+  const yCmp = a.position.y - b.position.y;
+
+  return yCmp !== 0 ? yCmp : a.position.x - b.position.x;
+}
+
+function checkCollisions(carts) {
+  return carts
+    .slice()
+    .sort(cmpCartPositions)
+    .reduce((collisionFound, cart, idx, arr) => {
+      if (collisionFound || idx < 1) {
+        return collisionFound;
+      }
+
+      const lastCart = arr[idx - 1];
+      return cmpCartPositions(cart, lastCart) !== 0 ? false : cart.position;
+    }, false);
+}
 
 function createCarts(map) {
   const cartDirections = {
@@ -19,10 +49,11 @@ function createCarts(map) {
   };
   const carts = [];
 
-  map.forEach((row, x) => {
-    row.forEach((col, y) => {
-      if (cartDirections[col]) {
-        carts.push(new Cart(map, { x, y }, cartDirections[col]));
+  map.forEach((col, y) => {
+    col.forEach((row, x) => {
+      if (cartDirections[row]) {
+        console.log(`\nNew cart: ${row} ${cartDirections[row]} at ${x},${y}`);
+        carts.push(new Cart(map, { x, y }, cartDirections[row]));
       }
     });
   });
--- solutions/13/cart.js
+++ solutions/13/cart.js
@@ -1,46 +1,98 @@
-const directions = [{ x: 0, y: -1 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 0 }];
+const directions = {
+  UP: { x: 0, y: -1 },
+  RIGHT: { x: 1, y: 0 },
+  DOWN: { x: 0, y: 1 },
+  LEFT: { x: -1, y: 0 }
+};
+
+const turns = {
+  LEFT: 'left',
+  RIGHT: 'right'
+};
+
+function isSameDirection(a, b) {
+  return a.x === b.x && a.y === b.y;
+}
+
+function turn(currentDirection, turn) {
+  if (isSameDirection(currentDirection, directions.UP)) {
+    return turn === turns.LEFT ? directions.LEFT : directions.RIGHT;
+  } else if (isSameDirection(currentDirection, directions.RIGHT)) {
+    return turn === turns.LEFT ? directions.UP : directions.DOWN;
+  } else if (isSameDirection(currentDirection, directions.DOWN)) {
+    return turn === turns.LEFT ? directions.RIGHT : directions.LEFT;
+  } else if (isSameDirection(currentDirection, directions.LEFT)) {
+    return turn === turns.LEFT ? directions.DOWN : directions.UP;
+  } else {
+    console.log('Unable to make a turn using', currentDirection, turn);
+  }
+}
 
 module.exports = class {
   constructor(map, position, direction) {
     this.map = map;
-    this.position = position;
-    this.direction = direction;
+    this.position = { ...position };
+    this.direction = { ...direction };
+    this.lastIntersectionChoice = null;
   }
 
-  decideNewDirection(position, lastPosition) {
-    return directions.find((dir) => {
-      const newPos = { x: position.x + dir.x, y: position.y + dir.y };
-      return (
-        newPos.x !== lastPosition.x &&
-        newPos.y !== lastPosition.y &&
-        this.map.data[newPos.y][newPos.x] !== ''
-      );
-    });
-  }
-
-  track() {
-    const foundLetters = [];
-    let stepsMade = 1; // first square counts
-    let position = this.map.getStart();
-    let direction = { x: 0, y: 1 }; // initial move is down
-    let canMove = true;
-    while (canMove) {
-      let lastPosition = position;
-      position = { x: position.x + direction.x, y: position.y + direction.y };
-      const square = this.map.data[position.y][position.x];
-      switch (square) {
-        case '+':
-          direction = this.decideNewDirection(position, lastPosition);
-          break;
-        default:
-          if (square.match(/[A-Z]/i)) {
-            foundLetters.push(square);
-          }
-      }
-      stepsMade++;
-      canMove = this.map.data[position.y + direction.y][position.x + direction.x] !== '';
+  move() {
+    const x = this.position.x + this.direction.x;
+    const y = this.position.y + this.direction.y;
+    if (!this.map[y] || !this.map[y][x]) {
+      console.log(`OOB: ${x},${y}`);
     }
+    this.position = { x, y };
+    this.changeDirection();
+    console.log('=>', this.position, this.direction);
+  }
 
-    return { foundLetters: foundLetters.join(''), stepsMade };
+  navigateIntersection() {
+    const outs = {
+      LEFT: 'left',
+      STRAIGHT: 'straight',
+      RIGHT: 'right'
+    };
+    switch (this.lastIntersectionChoice) {
+      case outs.LEFT:
+        this.lastIntersectionChoice = outs.STRAIGHT;
+        break;
+      case outs.STRAIGHT:
+        this.direction = turn(this.direction, turns.RIGHT);
+        this.lastIntersectionChoice = outs.RIGHT;
+        break;
+      case outs.RIGHT:
+      default:
+        this.direction = turn(this.direction, turns.LEFT);
+        this.lastIntersectionChoice = outs.LEFT;
+    }
+  }
+
+  changeDirection() {
+    const instruction = this.map[this.position.y][this.position.x];
+    switch (instruction) {
+      case '+':
+        this.navigateIntersection();
+        break;
+      case '\\':
+        // either turn left or right, depending on incoming direction
+        if (this.direction === directions.LEFT) {
+          this.direction = directions.UP;
+        } else {
+          this.direction = directions.RIGHT;
+        }
+        break;
+      case '/':
+        // either turn left or right, depending on incoming direction
+        if (this.direction === directions.RIGHT) {
+          this.direction = directions.UP;
+        } else {
+          this.direction = directions.LEFT;
+        }
+        break;
+      default:
+        // continue in same direction, handles - and |
+        break;
+    }
   }
 };
