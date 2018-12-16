566c4966babb1a77247b4e4711fe62738b6af142
Ola Christian Gundelsby <lug00ber@gmail.com>
2018-12-16 12:02:45 +0100

    Day 14, part 2 unverified

--- solutions/14.js
+++ solutions/14.js
@@ -4,20 +4,15 @@ let elves = [0, 1];
 const scoreboard = '37';
 
 console.log('[1]:', calcPart1(elves.slice(), scoreboard, Number.parseInt(input, 10), 10));
-console.log('[2]:', calcPart2(elves.slice(), scoreboard, input));
-// console.log('[2]:', calcPart2(elves.slice(), scoreboard, '51589'));
-// console.log('[2]:', calcPart2(elves.slice(), scoreboard, '01245'));
-// console.log('[2]:', calcPart2(elves.slice(), scoreboard, '92510'));
-// console.log('[2]:', calcPart2(elves.slice(), scoreboard, '59414'));
+// console.log('[2]:', calcPart2(elves.slice(), scoreboard, input));
+console.log('[2]:', calcPart2(elves.slice(), scoreboard, '51589'));
+console.log('[2]:', calcPart2(elves.slice(), scoreboard, '01245'));
+console.log('[2]:', calcPart2(elves.slice(), scoreboard, '92510'));
+console.log('[2]:', calcPart2(elves.slice(), scoreboard, '59414'));
 
 function calcPart1(elves, scoreboard, practiceRecipesCount, wantedRecipes) {
   while (scoreboard.length < practiceRecipesCount + wantedRecipes) {
-    const newScore = elves.reduce((sum, position) => {
-      return sum + Number(scoreboard[position]);
-    }, 0);
-
-    scoreboard = scoreboard.concat(newScore);
-
+    scoreboard += new String(Number(scoreboard[elves[0]]) + Number(scoreboard[elves[1]]));
     elves = elves.map((elf) => (1 + Number(scoreboard[elf]) + elf) % scoreboard.length);
   }
 
@@ -25,17 +20,14 @@ function calcPart1(elves, scoreboard, practiceRecipesCount, wantedRecipes) {
 }
 
 function calcPart2(elves, scoreboard, targetSequence) {
-  while (scoreboard.length < 21000000) {
-    const newScore = elves.reduce((sum, position) => {
-      return sum + Number(scoreboard[position]);
-    }, 0);
-    scoreboard = scoreboard.concat(newScore);
+  while (scoreboard.length < 21000) {
+    scoreboard += new String(Number(scoreboard[elves[0]]) + Number(scoreboard[elves[1]]));
     elves = elves.map((elf) => (1 + Number(scoreboard[elf]) + elf) % scoreboard.length);
   }
 
   const targetAt = scoreboard.indexOf(targetSequence);
   if (targetAt) {
-    return scoreboard.slice(0, targetAt).length;
+    return targetAt;
   }
 
   return `Unable to find ${targetSequence} in ${scoreboard}`;
