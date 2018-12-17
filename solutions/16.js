941dd3ea563073ba98b2f9ca7f8c0b685ab20092
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2018-12-17 17:25:31 +0100

    Day 16, part 2 still wip

--- solutions/16.js
+++ solutions/16.js
@@ -2,36 +2,105 @@ const CPU = require('./16/cpu.js');
 const fs = require('fs');
 
 const samples = parseSampleInput(fs.readFileSync('data/16-input-p1.txt', 'utf-8').split(/\r?\n/));
-const tripleFriends = findTriples(samples);
-console.log('[1]:', tripleFriends.length);
+const results = analyzeSamples(samples);
+console.log('[1]:', results.filter((sampleResult) => sampleResult.length > 2).length);
 
-const program = fs.readFileSync('data/16-input-p2.txt', 'utf-8');
+const program = fs
+  .readFileSync('data/16-input-p2.txt', 'utf-8')
+  .split(/\r?\n/)
+  .map((line) => line.split(' ').map(Number));
 
-function findTriples(samples) {
-  const cpu = new CPU();
-  const found = samples.map(({ before, command, after }) => {
-    let matches = 0;
-    for (let i = 0; i < cpu.operations.length; i++) {
-      cpu.setState(before);
-      cpu.execute(i, command[1], command[2], command[3]);
+const opCodeMap = mapOperations(samples, results);
+// console.log(opCodeMap[15]);
+// console.log('[2:', runProgram(program, opsMap));
 
-      if (
-        cpu.registers.every((val, idx) => {
-          return val === after[idx];
-        })
-      ) {
-        matches++;
+function mapOperations(samples, results) {
+  const opCodeMap = {};
+
+  samples.forEach((sample, idx) => {
+    const sampleOpCode = sample.command[0];
+    opCodeMap[sampleOpCode] = opCodeMap[sampleOpCode] || { testCases: [] };
+
+    opCodeMap[sampleOpCode].testCases.push(idx);
+  });
+
+  Object.keys(opCodeMap).forEach((opCode) => {
+    console.log(
+      `\n\nFinding cpu opcode for sample opcode ${opCode} (${
+        opCodeMap[opCode].testCases.length
+      } test cases)`
+    );
+    const passingCpuOpCodes = opCodeMap[opCode].testCases.reduce((passingOpCodes, current) => {
+      // console.log(`\nChecking test case #${current}`);
+      if (!passingOpCodes) {
+        // console.log(`Passed for ${current}:`, results[current]);
+        return results[current];
       }
 
-      if (matches > 2) {
-        break;
+      if (passingOpCodes.length === 0) {
+        // console.log('No opcodes to test, returning empty array');
+        return [];
+      }
+
+      // console.log('Previously passed:', passingOpCodes);
+      // console.log(`Passed for ${current}:`, results[current]);
+      return passingOpCodes.filter((opCode) => {
+        // console.log(`Checking opcode ${opCode}`, results[current].indexOf(opCode) > -1);
+        return results[current].indexOf(opCode) > -1;
+      });
+    }, null);
+
+    console.log('=> using', passingCpuOpCodes);
+    opCodeMap[opCode].cpuOpCode = passingCpuOpCodes;
+  });
+
+  while (
+    Object.keys(opCodeMap).filter((opCode) => {
+      return opCodeMap[opCode].cpuOpCode.length > 1;
+    }).length
+  ) {
+    const singles = Object.keys(opCodeMap)
+      .filter((opCode) => opCodeMap[opCode].cpuOpCode.length === 1)
+      .map((opCode) => opCodeMap[opCode].cpuOpCode[0]);
+
+    // remove singles from opcode lists with more than one entry
+  }
+
+  return opCodeMap;
+}
+
+function runProgram(program, opsMap) {
+  const cpu = new CPU();
+
+  program.forEach((command) => {
+    command[0] = opsMap[command[0]];
+    cpu.execute(...command);
+  });
+
+  return cpu.getState()[0];
+}
+
+function analyzeSamples(samples) {
+  const cpu = new CPU();
+  const validOperations = samples.map((sample) => {
+    let matches = [];
+    for (let i = 0; i < cpu.operations.length; i++) {
+      if (testSample(cpu, sample, i)) {
+        matches.push(i);
       }
     }
 
-    return matches > 2;
+    return matches;
   });
 
-  return found.filter((found) => !!found);
+  return validOperations;
+}
+
+function testSample(cpu, { before, command, after }, opId) {
+  cpu.setState(before);
+  cpu.execute(opId, command[1], command[2], command[3]);
+
+  return cpu.getState().every((val, idx) => val === after[idx]);
 }
 
 function parseSampleInput(lines) {
